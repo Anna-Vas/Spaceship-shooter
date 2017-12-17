f8d359de4a91443b586ca6a6b036075b348cbfac
IvanProtsenko <ivnprotsenko@gmail.com>
2017-12-17 17:27:27 +0300

    Update enemytype1.cpp

--- enemytype1.cpp
+++ enemytype1.cpp
@@ -1,8 +1,9 @@
 #include "enemytype1.h"
 
-EnemyType1::EnemyType1(int w,int h) : Enemy(w,h)
+EnemyType1::EnemyType1(int w,int h, int livesStep) : Enemy(w,h)
 {
-    lives = 1;
+    lives = 1+livesStep;
     speed = 1;
     price = 10;
+
 }

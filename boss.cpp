a28c818538e2004a5f3e21486f551885260f271a
IvanProtsenko <ivnprotsenko@gmail.com>
2017-12-25 02:11:04 +0300

    Update boss.cpp

--- boss.cpp
+++ boss.cpp
@@ -2,11 +2,11 @@
 #include <widget.h>
 #include <bullet.h>
 
-boss::boss(int w, int h)
+boss::boss(int w, int h, int livesStep)
 {
     point.setX(w/2);
     point.setY(0);
-    lives = 20;
+    lives = 20+livesStep*10;
     x = w;
 
 }
@@ -18,7 +18,7 @@ int boss::get_lives()
 
 void boss::draw(QPainter &painter)
 {
-    painter.drawImage(0, point.y(), QImage(":/img/boos4.png").scaled(2*260, 2*50));
+    painter.drawImage(0, point.y(), QImage(":/images/boos4.png").scaled(2*260, 2*50));
 }
 
 void boss::BossMinusLives()
@@ -29,7 +29,8 @@ void boss::BossMinusLives()
 Bullet boss::attack_1()
 {
     Bullet bul(point+QPoint(n,0),2);
-    n+=75;
+    n+=60;
     if(n>x/2) n = -x/2;
     return bul;
 }
+
